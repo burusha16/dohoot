c361a9c290cbf4c674aab20534805d3719269a26
burusha16 <burusha16@gmail.com>
2018-02-26 08:54:06 +0300

    Header, search and filter components

--- src/scripts/app.jsx
+++ src/scripts/app.jsx
@@ -1,11 +1,45 @@
 class App extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      searchFilter: {
+        city: '',
+        action: '',
+        keyword: ''
+      }
+    };
+  }
+
+  componentDidMount() {
+    let self = this;
+
+    window.ee.addListener('Search.add', function(rule) {
+      let newRule = self.state.searchFilter;
+      
+      for (let key in rule) {
+        if (rule[key]) {
+          newRule[key] = rule[key];
+        }
+      }
+      self.setState({searchFilter: newRule});
+    });
+  }
+
+  componentWillUnmount() {
+    window.ee.removeListener('Search.add');
+  }
+
   render() {
-    console.log('render');
+    let data = {};
+    data.cities = ['Dubai', 'Moscow', 'Kazan', 'London', 'New York'];
+    data.action = ['Education', 'Travel', 'Music', 'Apartments', 'Other'];
 
     return (
       <div className="app">
-        <h1> Hello world! </h1>
+        <Header />
+        <Search data = {data}/>
+        <Filter data = {data}/>
       </div>
     );
   }
